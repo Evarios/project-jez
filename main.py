92a4efe528002a6184aa810919358606dea10e37
Domektmp <dominik30062000@gmail.com>
2021-01-29 22:01:47 +0100

    Pierwszy Flask

--- main.py
+++ main.py
@@ -1,54 +1,70 @@
-import pylab
+from flask import Flask, render_template, request, redirect, url_for
+from bokeh.plotting import figure
+from bokeh.embed import components
+from bokeh.resources import CDN
 
-eta=0.95   #sprawnosc
-R=1     #opor lampy
-cv=1005 #powietrza
-a=5
-b=1.5
-h=0.4
-V=a*b*h
-ro = 1.21 #gestosc
-Tzad = 20 #zadana
-Tzew = 0 #otoczenia/początkowa
-#1 - boki, 2- front
-lambda1 = 0.16  #przenikalność cieplna materiału 
-lambda2 = 0.8
-d1=0.05 #grubość ścianki
-d2=0.01
-S1=a*h+2*b*h+a*b
-S2=a*h
-strata=S1*lambda1/d1+S2*lambda2/d2
-pom=cv*V*ro
+app = Flask(__name__)
 
-tp=0.01
-kp=0.1
-Ti=0.25
-Td=0.05
-N=100000
-T=Tzew
-e = [Tzad-Tzew]
+@app.route("/home", methods=["POST","GET"])
+def home():
+    if request.method=="POST":
+        eta=0.95   #sprawnosc
+        R=1     #opor lampy
+        #cv=1005 #powietrza
+        a=request.form["a"]
+        b=request.form["b"]
+        h=request.form["h"]
+        #V=a*b*h
+        #ro=1.21 #gestosc
+        Tzad=request.form["Tzd"] #zadana
+        Tzew=request.form["Tzew"] #otoczenia/początkowa
+        #1 - boki, 2- front
+        lambda1=request.form["lbd1"]  #przenikalność cieplna materiału 
+        lambda2=request.form["lbd2"]
+        d1=request.form["d1"] #grubość ścianki
+        d2=request.form["d2"]
+        S1=a*h+2*b*h+a*b
+        S2=a*h
+        strata=S1*lambda1/d1+S2*lambda2/d2
+        #pom=cv*V*ro
+        tp=0.01
+        kp=0.1
+        Ti=0.25
+        Td=0.05
+        N=100000
+        T=Tzew
+        e=[Tzad-Tzew]
+        Qdmax = 64
+        umax = 10
+        a=Qdmax/umax
+        x=[0]
+        y=[Tzew]
+        n=1
+        while n<=N:
+            e.append(Tzad-T)
+            u=kp*(e[-2]+(tp/Ti)*sum(e)+(Td/tp)*(e[-1]-e[-2]))
+            #if (u > umax):
+            #    u = umax
+            Qd=kp*u
+            if (Qd > 230):
+                Qd = 230
+            #print(e[n],u, "\n")
+            T=(eta*(Qd**2)*tp/R-tp*(T-Tzew)*strata)/(tp*strata)+T
+            #print(n,T)
+            if T>45: #T>tmax
+                T=45
+                #e[-1]=Tzad-T
+            x.append(n*tp)
+            y.append(T)
+            n+=1
+        p=figure()
+        p.line(x,y)
+        script,div=components(p)
+        cdn_js=CDN.js_files
+        cdn_css=CDN.css_files
+        return render_template("tmp.html", script1=script, div1=div, cdn_css=cdn_css, cdn_js=cdn_js)
+    else:
+        return render_template("index.html")
 
-n=1
-x=[0]
-y=[Tzew]
-
-while n<=N:
-  e.append(Tzad-T)
-  u=kp*(e[-2]+(tp/Ti)*sum(e)+(Td/tp)*(e[-1]-e[-2]))
-  Qd=kp*u
-  #print(e[n],u, "\n")
-
-  T=(eta*(Qd**2)*tp/R-tp*(T-Tzew)*strata)/(tp*strata)+T
-  #print(n,T)
-  #if T>300: #T>tmax
-   # T=300
-    #e[-1]=Tzad-T
-
-  x.append(n*tp)
-  y.append(T)
-  n+=1
-
-pylab.plot(x,y)
-
-pylab.show()
-#test githuba
+if __name__ == "__main__":
+    app.run(debug=True)
