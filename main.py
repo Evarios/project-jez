48b7043eca42a53cbf883fea760b43744ef213a9
milkosek <77544128+milkosek@users.noreply.github.com>
2021-02-21 19:00:32 +0100

    Update main.py
    Zaktualizowany wzór + dane. Bierze też pod uwagę temperaturę nocy. Wykres na cały dzień.

--- main.py
+++ main.py
@@ -10,38 +10,46 @@ def home():
     if request.method=="POST":
         eta=0.95   #sprawnosc
         R=1     #opor lampy
-        #cv=1005 #powietrza
-        a=float(request.form["glebokosc"])
-        b=float(request.form["szerokosc"])
-        h=float(request.form["wysokosc"])
-        #V=a*b*h
-        #ro=1.21 #gestosc
+        cv=1005 #powietrza
+        a=float(request.form["glebokosc"])/100
+        b=float(request.form["szerokosc"])/100
+        h=float(request.form["wysokosc"])/100
+        V=a*b*h
+        ro=1.21 #gestosc
         Tzad=float(request.form["temperatura_dzien"]) #zadana
         Tzew=float(request.form["temperatura_otoczenia"]) #otoczenia/początkowa
+        Tnoc=float(request.form["temperatura_noc"]) #temp w nocy
         #1 - boki, 2- front
         lambda1=float(request.form["material_scianek"])  #przenikalność cieplna materiału 
         lambda2=float(request.form["material_frontu"])
-        d1=float(request.form["grubosc_scianek"]) #grubość ścianki
-        d2=float(request.form["grubosc_frontu"])
+        d1=float(request.form["grubosc_scianek"])/1000 #grubość ścianki
+        d2=float(request.form["grubosc_frontu"])/1000
         S1=a*h+2*b*h+a*b
         S2=a*h
         strata=S1*lambda1/d1+S2*lambda2/d2
-        #pom=cv*V*ro
-        tp=0.01
-        kp=0.1
+        pom=cv*V*ro
+        tp=1
+        kp=0.01
         Ti=0.25
         Td=0.05
-        N=100000
+        N=86400
+        if(Tzew > Tzad):
+            Tzad = Tzew
         T=Tzew
         e=[Tzad-Tzew]
-        Qdmax = 64
-        umax = 10
+        temp = Tzad-T
+        Qdmax = 24
+        umax = 20
         a=Qdmax/umax
         x=[0]
         y=[Tzew]
         n=1
         while n<=N:
-            e.append(Tzad-T)
+            if(n>43200):
+                temp = Tnoc - T
+            else:
+                temp = Tzad - T
+            e.append(temp)
             u=kp*(e[-2]+(tp/Ti)*sum(e)+(Td/tp)*(e[-1]-e[-2]))
             #if (u > umax):
             #    u = umax
@@ -49,7 +57,10 @@ def home():
             if (Qd > 230):
                 Qd = 230
             #print(e[n],u, "\n")
-            T=(eta*(Qd**2)*tp/R-tp*(T-Tzew)*strata)/(tp*strata)+T
+            if (Qd < 0):
+                T=(eta*(Qd**2)*tp/R-tp*(T-Tzew)*strata)/(pom)+ T
+            else:
+                T=(eta*(Qd**2)*tp/R-tp*(T-Tzew)*strata)/(pom)+ T
             #print(n,T)
             if T>45: #T>tmax
                 T=45
