9aa525a8d3351a872d1d79c06324320fb2c343c8
Domektmp <dominik30062000@gmail.com>
2021-02-25 18:53:57 +0100

    Fixed: data loss on generation

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from flask import Flask, render_template, request
 from bokeh.plotting import figure
 from bokeh.embed import components
 from bokeh.resources import CDN
-import random
+from random import uniform
+
 
 app = Flask(__name__)
 
@@ -23,8 +24,8 @@ def home():
         #1 - boki, 2- front
         lambda1=float(request.form["material_scianek"])  #przenikalność cieplna materiału 
         lambda2=float(request.form["material_frontu"])
-        d1=float(request.form["grubosc_scianek"])/1000 #grubość ścianki
-        d2=float(request.form["grubosc_frontu"])/1000
+        d1=float(request.form["grubosc_scianek"])/100 #grubość ścianki
+        d2=float(request.form["grubosc_frontu"])/100
         S1=a*h+2*b*h+a*b
         S2=a*h
         strata=S1*lambda1/d1+S2*lambda2/d2
@@ -43,7 +44,7 @@ def home():
         temp = Tzad-T
         Qdmax = 24
         umax = 20
-        a = (Qdmax/umax)/100
+        akier = (Qdmax/umax)/100
         x = [0]
         y = [Tzew]
         k = [1/3600,43200/3600,43201/3600,86399/3600,86400/3600,129600/3600,129601/3600,172200/3600]
@@ -52,7 +53,7 @@ def home():
         esum = e[0]
         while (n<=N):
             if (n%36 == 0):
-                Tzew += round(random.uniform(-0.02,0.02),4)
+                Tzew += round(uniform(-0.02,0.02),4)
             if (n%86400>43200):
                 temp = Tnoc - T
             else:
@@ -62,7 +63,7 @@ def home():
             u=kp*(e[-2]+(tp/Ti)*esum+(Td/tp)*(e[-1]-e[-2]))
             #if (u > umax):
             #    u = umax
-            Qd=a*u
+            Qd=akier*u
             if (Qd > 230):
                 Qd = 230
             #print(e[n],u, "\n")
@@ -88,7 +89,29 @@ def home():
         script,div=components(p,CDN)
         cdn_js=CDN.js_files
         cdn_css=CDN.css_files
-        return render_template("bokeh.html",script1=script, div1=div, cdn_css=cdn_css, cdn_js=cdn_js[0])
+        a *= 100
+        b *= 100
+        h *= 100
+        d1 *= 100
+        d2 *= 100
+        if (a.is_integer()):
+            a = int(a)
+        if (b.is_integer()):
+            b = int(b)
+        if (h.is_integer()):
+            h = int(h)
+        if (d1.is_integer()):
+            d1 = int(d1)
+        if (d2.is_integer()):
+            d2 = int(d2)
+        if (Tzew.is_integer()):
+            Tzew = int(Tzew)
+        if (Tzad.is_integer()):
+            Tzad = int(Tzad)
+        if (Tnoc.is_integer()):
+            Tnoc = int(Tnoc)
+        return render_template("bokeh.html",script1=script, div1=div, cdn_css=cdn_css, cdn_js=cdn_js[0], glebokosc=a, szerokosc=b, 
+        wysokosc=h, grubosc_scianek=d1, grubosc_frontu=d2, temperatura_dzien=Tzad, temperatura_otoczenia=format(Tzew,'.1f'), temperatura_noc=Tnoc)
     else:
         return render_template("index.html")
 
